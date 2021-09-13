8bb7e17f30ce39f8b991848aecf2855b5e4ec3a4
= <=>
2021-09-13 09:34:49 +0700

    templatedrivenform

--- src/app/Service/common.service.ts
+++ src/app/Service/common.service.ts
@@ -11,4 +11,7 @@ export class CommonService {
    public binhPhuong(n : number): number{
      return n * n ;
    }
+   public submitData(data : any) : void{
+     console.log('gửi data lên server. Data :', data );
+   }
 }
